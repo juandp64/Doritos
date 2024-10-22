6b7d14ee50b51fbb812613fc5346929378952b61
juanda64 <juandavpenilla@gmail.com>
2024-10-22 13:59:17 -0500

    se creo el registro front

--- DoritosFront/src/App.jsx
+++ DoritosFront/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Register from './components/Register';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
--- DoritosFront/src/components/Login.jsx
+++ DoritosFront/src/components/Login.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 import '../styles/login.css';
+import Logo from '../img/DoritosLogo.png'; // Asegúrate de usar el nombre 'Logo'
 
 export default function Login() {
   return (
     <div className="login-container">
       <h2>Login</h2>
       <form>
+        <div className="logo-container">
+          <img src={Logo} alt="Logo" className="login-logo" />
+        </div>
         <div className="mb-3">
           <input type="text" className="form-control" id="username" placeholder="Correo electrónico" />
         </div>
@@ -14,8 +19,8 @@ export default function Login() {
         </div>
         <button type="submit" className="btn btn-primary mt-3">Iniciar sesión</button>
       </form>
-      <div className="register-link">
-        <p>¿No tienes cuenta? <a href="#">Registrarse</a></p>
+      <div className="register-link mt-3">
+        <Link to="/register" className="btn btn-link">¿No tienes cuenta? Regístrate</Link>
       </div>
     </div>
   );
